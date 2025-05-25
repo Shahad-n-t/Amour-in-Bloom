431ae6c53af81fc4c15e9069bad50e63ed49a3fe
Shahad-n-t <s.n.turkistani@gmail.com>
2025-05-25 22:40:23 +0300

    made navbar

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const header = () => {
+  return (
+   <header>
+    <section>
+        <div className='header-banner'>
+            <h1>"The Language of Flowers, Spoken Fluently"</h1>
+        </div>
+    </section>
+   </header>
+  )
+}
+
+export default header
---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import logo from "../components/images/headers/logo.jpg"
+
+const Navbar = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
+    <div className='menu-icon' onClick={toggleMenu}>
+            <div className='bar'></div>
+            <div className='bar'></div>
+            <div className='bar'></div>
+        </div>
+
+
+
+  return (
+    <nav className={`navbar ${menuOpen? "open" : ""}`}>
+      <a href='/' className='logo'>
+      <img src={logo} alt='Amour in Bloom logo'/>
+      </a>
+      
+
+      <ul className={`nav-links ${menuOpen ? "visible" : "" }`}>
+        <li>
+            <a href='/'>Home</a>
+        </li>
+        <li>
+            <a href='/'>About</a>
+        </li>
+        <li>
+            <a href='/'>Contact Us</a>
+        </li>
+        <li>
+            <a href='/'>Categories</a>
+        </li>
+        <li>
+            <a href='/'>Shopping Cart</a>
+        </li>
+        <li>
+            <a href='/'>Login</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
